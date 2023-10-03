db407d1f6c5e581ad47aec70477c274ab96ab7d7
Manuel Fellner <mfellner@student.tgm.ac.at>
2023-10-03 14:01:59 +0200

    feat(auth): adding authentication first draft"

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, Flask
+from flask import Blueprint, render_template, Flask, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 
@@ -13,16 +13,18 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/app')
 def app():
     return render_template('app.html')
 
-class User(db.Model):
 
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -30,7 +32,42 @@ class User(db.Model):
     password = db.Column(db.String(100))
     favourite_color = db.Column(db.String(100))
 
+
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+@login_manager.user_loader
+def loader_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == 'POST':
+        user = User(email=request.form.get('email'),
+                    password=request.form.get('password'),
+                    favorite_color=request.form.get('color'))
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(
+            email=request.form.get('email')).first()
+
+        if user.password == request.form.get('password'):
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run()
